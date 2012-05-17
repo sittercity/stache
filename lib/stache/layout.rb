7a180ed0ff1df8985ff9ce5fbec3c6e6accc84f9
Jeremy Bush <contractfrombelow@gmail.com>
2012-05-17 12:55:09 -0500

    Add ability to change layouts for a layout class

--- lib/stache/layout.rb
+++ lib/stache/layout.rb
@@ -4,13 +4,19 @@ module Stache
   # This class is for providing layouts in a better way.
   # Your page class should extend this class.
   class Layout < ::Stache::View
+    attr_writer :layout
+
+    def layout
+      @layout ||= :layout
+    end
+
     # template here would be the pages' template, not the layout.
     def render(data = template, ctx = {})
       # Store the current template, we'll need to stuff it inside the layout
       page_template = data
 
       # Grab the layout template, to render it at the end
-      layout = partial(:layout)
+      layout_template = partial(layout)
 
       # Render the page_template using this class's context
       # (which will be combined with the layout context)
@@ -18,7 +24,7 @@ module Stache
 
       # stick that rendered template as :yield into the layout template
       # (which will be combined with the current context)
-      super(layout, :yield => rendered_template)
+      super(layout_template, :yield => rendered_template)
     end
   end
 end
